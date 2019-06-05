f38c63b4101771db3a2c9fe2ab9b3c76a4b0a38e
oyoyoa <>
2019-06-05 10:15:20 +0900

    initial commit

--- main/views.py
+++
@@ -1,7 +0,0 @@
-from django.views.generic import TemplateView
-from django.shortcuts import render
-
-
-# Create your views here.
-class ModelView(TemplateView):
-    template_name = "main/index.html"
